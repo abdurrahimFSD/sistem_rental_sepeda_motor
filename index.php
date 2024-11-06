7060e26bec70eaa1aec3e94d6fe5a1ad3e50be28
Abdurrahim <abdurrahim22code@gmail.com>
2024-11-06 19:11:08 +0800

    Menambahkan routing

--- index.php
+++ index.php
@@ -3,15 +3,44 @@ include ('./layouts/mainWrapper.php');
 include ('./layouts/sidebar.php');
 include ('./layouts/pageWrapper.php');
 
-
-
+error_reporting(0);
 switch ($_GET['page']) {
     case 'home':
         include ('./home.php');
         break;
+
+    case 'sepedaMotorData':
+        include ('./pages/motor/motorRead.php');
+        break;
+    case 'sepedaMotorCreate':
+        include ('./pages/motor/motorCreate.php');
+        break;
+    case 'sepedaMotorUpdate':
+        include ('./pages/motor/motorUpdate.php');
+        break;
+    
+    case 'penyewaData':
+        include ('./pages/penyewa/penyewaRead.php');
+        break;
+    case 'penyewaCreate':
+        include ('./pages/penyewa/penyewaCreate.php');
+        break;
+    case 'penyewaUpdate':
+        include ('./pages/penyewa/penyewaUpdate.php');
+        break;
+
+    case 'sewaData':
+        include ('./pages/sewa/sewaRead.php');
+        break;
+    case 'sewaCreate':
+        include ('./pages/sewa/sewaCreate.php');
+        break;
+    case 'sewaUpdate':
+        include ('./pages/sewa/sewaUpdate.php');
+        break;
+    
     default:
         include ('./home.php');
-        break;
 }
 
 
