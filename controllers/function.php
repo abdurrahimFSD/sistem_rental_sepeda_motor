d202d354b84a5a4a9e98a7893c0d675e4b687fcd
Abdurrahim <abdurrahim22code@gmail.com>
2024-11-07 19:02:52 +0800

    Menambahkan function fetchData

--- controllers/function.php
+++ controllers/function.php
@@ -8,4 +8,25 @@ if (file_exists('./config/connection.php')) {
     die('Connection file not found');
 }
 
+// Function fecthData untuk mengambil semua data dari table
+function fetchData($tableName) {
+    global $connection;
+
+    // Membuat query SQL untuk memilih semua data dari table yg namanya diberikan melalui parameter $tableName
+    $queryFecthData = "SELECT * FROM $tableName";
+
+    // Menjalankan query SQL
+    $resultFetchData = mysqli_query($connection, $queryFecthData);
+
+    // Menyiapkan array kosong untuk menyimpan data dari query
+    $data = [];
+
+    // Mengambil setiap baris data dari query dan menambahkannya ke dalam array $data
+    while ($row = mysqli_fetch_assoc($resultFetchData)) {
+        $data[] = $row;
+    }
+
+    // Mengembalikan array $data yg berisi semua baris data dari table
+    return $data;
+}
 ?>
