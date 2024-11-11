fa676e883a1e81c13413fff276e4be019d79ceb0
Abdurrahim <abdurrahim22code@gmail.com>
2024-11-11 21:53:32 +0800

    Menambahkan logika untuk memproses simpan data motorCreate

--- controllers/process.php
+++ controllers/process.php
@@ -2,4 +2,20 @@
 
 include('./function.php');
 
+// Mengecek apakah form telah disubmit
+if (isset($_POST['simpan'])) {
+
+    // Jika tombol simpan adalah sepedaMotorCreate
+    if ($_POST['simpan'] == 'sepedaMotorCreate') {
+        // Memanggil function motorCreate
+        $result = motorCreate($_POST);
+
+        // Jika proses berhasil
+        if ($result == 'success') {
+            return 'successMotorCreate';
+        } else {
+            return 'errorMotorCreate';
+        }
+    }
+}
 ?>
