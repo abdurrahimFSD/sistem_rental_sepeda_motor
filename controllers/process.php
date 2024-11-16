5ef7fe5f2258d18d73ae262bf2aee2ef4d81f430
Abdurrahim <abdurrahim22code@gmail.com>
2024-11-16 11:04:45 +0800

    Menambahkan logika untuk memproses penghapusan data motor

--- controllers/process.php
+++ controllers/process.php
@@ -32,4 +32,17 @@ if (isset($_POST['simpan'])) {
         }
     }
 }
+
+// Mengecek apakah parameter 'id_motor' ada di URL
+if (isset($_GET['id_motor'])) {
+    // Memanggil function motorDelete
+    $result = motorDelete($_GET['id_motor']);
+
+    // Jika proses berhasil
+    if ($result == 'success') {
+        echo 'successMotorDelete';
+    } elseif ($result == 'error') {
+        echo 'errorMotorDelete';
+    }
+}
 ?>
