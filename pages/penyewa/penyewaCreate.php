90775d76a3089518f34a5d885a450dace9c8f085
Abdurrahim <abdurrahim22code@gmail.com>
2024-11-18 20:50:26 +0800

    Menambahkan halaman tambah data penyewa

---
+++ pages/penyewa/penyewaCreate.php
@@ -0,0 +1,63 @@
+<!-- Start Body Wrapper -->
+<div class="body-wrapper">
+    <div class="container-fluid">
+
+        <!-- Start Breadcrumb -->
+        <div class="card card-body py-3">
+            <div class="row align-items-center">
+                <div class="col-12">
+                    <div class="d-sm-flex align-items-center justify-space-between">
+                        <h4 class="mb-4 mb-sm-0 card-title">Tambah Data Penyewa</h4>
+                        <nav aria-label="breadcrumb" class="ms-auto">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item d-flex align-items-center">
+                                    <a class="text-muted text-decoration-none d-flex" href="./">
+                                        <iconify-icon icon="solar:home-2-line-duotone" class="fs-6"></iconify-icon>
+                                    </a>
+                                </li>
+                                    <iconify-icon icon="ic:sharp-keyboard-arrow-right" class="mx-1" width="26" height="26"></iconify-icon>
+                                <li class="breadcrumb-item my-auto" aria-current="page">
+                                    <span class="badge fw-medium fs-2 bg-primary-subtle text-primary-emphasis"> Tambah Penyewa </span>
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Breadcrumb -->
+
+        <div class="widget-content searchable-container list">
+
+            <!-- Start Card body main content -->
+            <div class="card card-body">
+                <h4 class="card-title">Masukkan Data Penyewa</h4>
+                <hr class="mb-4">
+                <form id="penyewaCreateForm">
+                    <div class="mb-3">
+                        <label for="namaPenyewa" class="form-label">Nama Penyewa</label>
+                        <input type="text" class="form-control" name="namaPenyewa" id="namaPenyewa" placeholder="Erling Haaland" required>
+                    </div>
+                    <div class="mb-3">  
+                        <label for="nomorTelepon" class="form-label">Nomor Telepon</label>
+                        <input type="text" class="form-control" name="nomorTelepon" id="nomorTelepon" placeholder="081311112222" required>
+                    </div>
+                    <div class="mb-3">  
+                        <label for="alamat" class="form-label">Alamat</label>
+                        <input type="text" class="form-control" name="alamat" id="alamat" placeholder="Norway" required>
+                    </div>
+                    <a href="?page=penyewaData" class="d-inline-flex justify-content-center align-items-center btn btn-outline-muted me-2">
+                        <iconify-icon icon="fluent:arrow-left-24-filled" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Kembali
+                    </a>
+                    <input type="hidden" name="simpan" value="penyewaCreate">
+                    <button type="submit" class="d-inline-flex justify-content-center align-items-center btn btn-primary">
+                        <iconify-icon icon="fluent:save-24-regular" class="me-1 fs-5 d-inline-flex align-items-center"></iconify-icon>Simpan
+                    </button>
+                </form>
+            </div>
+            <!-- ENd Card body main content -->
+        </div>
+    </div>
+    
+</div>
+<!-- End Body Wrapper -->
